3bcece3d5d8ac6d7421f54d8bb81589f0db9428d
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-09-24 13:27:39 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-
 import Phaser from "phaser";
+import PlayScene from "./scenes/playScene";
+
+const WIDTH = 800
+const HEIGHT = 600
+const BIRD_POSITION = {
+  x: WIDTH / 10,
+  y: HEIGHT / 2
+}
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION
+}
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
   physics: {
     default: 'arcade',
     arcade: {
@@ -12,11 +24,7 @@ const config = {
       debug: true,
     },
   },
-  scene: {
-    preload,
-    create,
-    update,
-  }
+  scene: [new PlayScene(SHARED_CONFIG)]
 };
 
 new Phaser.Game(config);
