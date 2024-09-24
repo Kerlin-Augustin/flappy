55ac6badb8ef81bfa15910ed5b9e0dd3aa89b2e6
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-09-24 15:19:06 -0400

    added collider

--- src/scenes/playScene.js
+++ src/scenes/playScene.js
@@ -29,7 +29,7 @@ class PlayScene extends Phaser.Scene {
     this.createBG();
     this.createBird();
     this.createPipes();
-
+    this.createColliders();
     this.handleInputs();
   }
 
@@ -60,6 +60,10 @@ class PlayScene extends Phaser.Scene {
     this.pipes.setVelocityX(this.pipesVelocity)
   }
 
+  createColliders(){
+    this.physics.add.collider(this.bird, this.pipes, this.gameOver, null, this);
+  }
+
   handleInputs() {
     this.input.on('pointerdown', this.flap, this);
     this.input.keyboard.on('keydown-SPACE', this.flap, this);
@@ -67,7 +71,7 @@ class PlayScene extends Phaser.Scene {
 
   checkGameStatus(){
     if (this.bird.y >= this.config.height - this.bird.height || this.bird.y + this.bird.height <= 0) {
-      this.restartBirdPosition()
+      this.gameOver()
     }
   }
 
@@ -105,7 +109,7 @@ class PlayScene extends Phaser.Scene {
     return rightMostX
   }
 
-  restartBirdPosition() {
+  gameOver() {
     this.bird.x = this.config.startPosition.x
     this.bird.y = this.config.startPosition.y
     this.bird.body.velocity.y = 0
